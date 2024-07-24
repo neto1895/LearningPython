23e680f4bafd34b36b3ad13becf50be22b590660
neto1895 <cruz.rosales.ernesto@gmail.com>
2024-07-24 07:13:53 -0600

    more exercises

--- samples.py
+++ samples.py
@@ -1,5 +1,32 @@
-print("test")
+planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune']
+for planet in planets:
+    print(planet, end=' ') # print all on same line
 
-a=1
-b=2
-print(a+b)
+print("")
+
+multiplicands = (2, 2, 2, 3, 3, 5)
+product = 1
+for mult in multiplicands:
+    product = product * mult
+print(product)
+
+print("")
+
+s = 'steganograpHy is the practicE of conceaLing a file, message, image, or video within another fiLe, message, image, Or video.'
+msg = ''
+# print all the uppercase letters in s, one at a time
+for char in s:
+    if char.isupper():
+        print(char, end='')
+
+print("")
+
+for i in range(5):
+    print("Doing important work. i =", i)
+
+print("")
+
+def count_negatives(nums):
+    return len([num for num in nums if num < 0])
+print(count_negatives([1,-2,-1,-3,0,22]))
+
